a1432578517175d64dc7db14004982e911a1e139
EasyValley <EasyValley@users.noreply.github.com>
2019-06-24 11:42:51 +0800

    函数调用自身，称为递归。如果尾调用自身，就称为尾递归。用于减少内存

---
+++ factorial.js
@@ -0,0 +1,10 @@
+function tailFactorial(n, total) {
+  if (n === 1) return total;
+  return tailFactorial(n - 1, n * total);
+}
+
+function factorial(n) {
+  return tailFactorial(n, 1);
+}
+
+factorial(5) // 120
