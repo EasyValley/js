12aa97186a0be999db26bc276035ecfc47edb2e9
EasyValley <EasyValley@users.noreply.github.com>
2019-12-09 11:49:02 +0800

    Update bind.js

--- bind.js
+++ bind.js
@@ -1,19 +1,36 @@
+ // 方式一
+  Function.prototype.bindT = function(that, ...args) {
+    return (...args2) => {
+      this.apply(that, args.concat(args2));
+    };
+  };
 
-    function bind(context, fn) {
-        return (...args) => {
-            return fn.apply(context, args);
-        }
-    }
+  function f(a, b) {
+    console.log(this);
+    console.log(a + "===" + b);
+  }
+  let obj = {
+    name: "zs"
+  };
+  let f2 = f.bindT(obj, "h");
+  f2("ooo");
 
-    var obj = {
-        name: 'zs'
-    }
+  // 方式二
+  function bind(context, fn, ...args0) {
+    return (...args) => {
+      return fn.apply(context, args0.concat(args));
+    };
+  }
 
-    function fn(a, b) {
-        console.log(a + '===' + b)
-        console.log(this)
-    }
+  let obj = {
+    name: "zs"
+  };
 
-    var bf = bind(obj, fn)
+  function fn(a, b) {
+    console.log(a + "===" + b);
+    console.log(this);
+  }
 
-    bf(22, 99)
+  var bf = bind(obj, fn, 66);
+
+  bf(22, 99);
